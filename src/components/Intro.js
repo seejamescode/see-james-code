b32c528a562766ebac92824a1ce2f7d8b409d2db
James Y. Rauhut <jamesrauhut@us.ibm.com>
2016-08-28 21:33:54 -0500

    Removing about paragraph widow

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -15,7 +15,6 @@ export class Intro extends Component {
         >
           ATX Designer working for IBM Design.
           I love coding, researching, and trying my best for God.
-          <br />
           Reach me by <a className={styles.link} href="mailto:james@seejamescode.com">email</a> or <a className={styles.link} href="https://twitter.com/seejamescode">Twitter</a>.
         </p>
       </div>
