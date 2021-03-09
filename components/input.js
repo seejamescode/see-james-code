7811c6e934a737406e984603a56c885d57fa5c83
seejamescode <james@seejamescode.com>
2021-03-08 21:51:11 -0600

    fix(search): dark mode input color

--- components/input.js
+++ components/input.js
@@ -4,6 +4,7 @@ const Input = styled.input`
   appearance: none;
   background: none;
   border: none;
+  color: ${({ theme }) => theme.colors.font};
   flex: 1;
   font-size: ${({ theme }) => theme.type.small.size};
   line-height: ${({ theme }) => theme.type.small.line};
